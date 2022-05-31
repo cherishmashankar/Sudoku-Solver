455aee518b0b928f77fa3e371b9109000d0c80b3
Cherishma <sha@biba.uni-bremen.de>
2022-05-31 18:55:28 +0200

    predict_classes() updated

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -56,11 +56,12 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          verbose=1,    
+          verbose=1,
           validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
 
 model.save('mnist_model.h5')
 model.save_weights('mnist_model_weights.h5')
